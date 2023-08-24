cfd006339624c74478bec3a921d6e5e175498927
Carlos A <csamillanm@gmail.com>
2023-08-24 12:36:17 -0500

    Agregar archivos de proyecto.

---
+++ PlatziEntityFramework/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace PlatziEntityFramework.Model
+{
+    public class Category
+    {
+        public Guid CategoryID { get; set; }   
+        
+        public string Name { get; set; }    
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+
+    }
+}
---
+++ PlatziEntityFramework/Model/task.cs
@@ -0,0 +1,29 @@
+﻿namespace PlatziEntityFramework.Model
+{
+    public class task
+    {
+        public Guid TaskId { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public DateTime DateCreate { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
+
+public enum Priority
+{
+    Low,
+
+    Medium, 
+    
+    High
+}
